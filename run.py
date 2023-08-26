8e54f59c9acf412913a83b854193ea434843893f
Edouard102 Trentesaux <edouard102@gmx.fr>
2023-08-26 17:12:15 +0000

    add fontion handle guess

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
 
-
 import random
 from random import randint 
 
@@ -12,11 +11,11 @@ def create_grid(rows, cols):
 
     grid = [['o' for _ in range(cols + 1)] for _ in range(rows + 1)] 
 
-    # Set row headers (latitude indices)
+# Set row headers (latitude indices)
     for i in range(1, rows + 1):
         grid[i][0] = str(i)  
 
-    # Set column headers (longitude indices)
+# Set column headers (longitude indices)
     for j in range(1, cols + 1):
         grid[0][j] = str(j)  
     return grid
@@ -28,53 +27,8 @@ def display_grid(grid):
     for row in grid:
         print(' '.join(row))
         
-def start_game():
-    """
-    Starts the Battleship game by guiding the player through the initial steps.
-    """
-# display title and ask player name
-    print("Battleship")
-    print("Enter your name to start the game")
-    print("David")
-
-    player_name= input("Enter your data here: ")
-    print(f"Welcome, {player_name} :)")
-
-# Display the game rules
-    print("\nRules of the Game:")
-    print("1. Battleship is a two-player game.")
-    print("2. Each player will have a random placement for these ships on his grid.")
-    print("3. Players take turns guessing the coordinates to target the opponent's ships.")
-    print("4. The first player to sink all of the opponent's ships wins.")
-    
-    input("Press Enter to start the game...")
-
-# display grid for player and computer
-    player_grid = create_grid(5, 5)
-    
-    computer_grid = create_grid(5, 5)
-
-    print("\nHere's your game grid:")
-    display_grid(player_grid)
-
-    print("\nHere's the computer's game grid:")
-    display_grid(computer_grid)
-# Define the number of the ships   
-    num_ships = 4
-# Define the length of the ships
-    ship_length = 2  
-    for _ in range(num_ships):
-        place_ship(player_grid, ship_length)
-
-    for _ in range(num_ships):
-        place_ship(computer_grid, ship_length)
-
-#play_game(player_grid, computer_grid, num_ships)
-
-#The actual game logic would be add here
-# This would include player turns,  4 random ship placement , shooting, score rest game etc.
-
 def place_ship(grid, length):
+
     """
     Place a ship of given length randomly on the player and computer grid.
     """
@@ -88,13 +42,73 @@ def place_ship(grid, length):
             col = random.randint(1, len(grid[0]) - 1)
         
         # Check if the positions are available
-        if all(grid[row + (i if orientation == 'vertical' else 0)][col + (i if orientation == 'horizontal' else 0)] == 'O'
+        if all(grid[row + (i if orientation == 'vertical' else 0)][col + (i if orientation == 'horizontal' else 0)] == 'o'
                for i in range(length)):
             for i in range(length):
                 grid[row + (i if orientation == 'vertical' else 0)][col + (i if orientation == 'horizontal' else 0)] = '-'
             break
 
+def start_game():
+    """
+    Starts the Battleship game by guiding the player through the initial steps.
+    """
+# display title and ask player name
+    print("Battleship")
+    print("Enter your name to start the game")
+
+    player_name= input("Enter your Name here: ")
+    print(f"Welcome, {player_name} :)")
+
+# Display the game rules
+    print("\nRules of the Game:")
+    print("1. Battleship is a two-player game.")
+    print("2. Each player will have a random placement for these ships on his grid.")
+    print("3. Players take turns guessing the coordinates to target the opponent's ships.")
+    print("4. The first player to sink all of the opponent's ships wins.")
+    
+    input("Press Enter to start the game...")
+
+# display grid for player and computer
+player_grid = create_grid(5, 5)
+    
+computer_grid = create_grid(5, 5)
+
+print("\nHere's your game grid:")
+display_grid(player_grid)
+
+print("\nHere's the computer's game grid:")
+display_grid(computer_grid)
+
+# Define the number of the ships   
+num_ships = 4
+    
+# Define the length of the ships
+ship_length = 2  
+
+# Place ships on player and computer grids
+for _ in range(num_ships):
+    place_ship(player_grid, ship_length)
+
+for _ in range(num_ships):
+    place_ship(computer_grid, ship_length)
+
+def handle_guess(grid, guess_row, guess_col):
+    '''
+    handling attacks and determining whether a shot has hit a ship or not
+    '''
+
+    if grid[guess_row][guess_col] == 'ship':
+
+        grid[guess_row][guess_col] = 'hit'  
+        return 'hit'  
+    elif grid[guess_row][guess_col] == 'empty':
+        grid[guess_row][guess_col] = 'miss'  
+        return 'miss'  
+    else:
+        return 'already_guessed' 
+
 def play_game(player_grid, computer_grid, num_ships):
+
     """
     Main logic handling player and computer attacks, determines winner.
     """
@@ -106,9 +120,12 @@ def play_game(player_grid, computer_grid, num_ships):
     while player_ships_remaining > 0 and computer_ships_remaining > 0:
         if current_player == 'player':
             print("\nPlayer's Turn")
+            #between 1 to 5
             guess_row = int(input("Guess Row: "))
+            #between 1 to 5 ajouter cette phrance
             guess_col = int(input("Guess Col: "))
             result = handle_guess(computer_grid, guess_row, guess_col)
+# ajouterfonction pour verifier erreur
 
             if result == 'hit':
                 print("You hit an enemy ship!")
@@ -131,7 +148,7 @@ def play_game(player_grid, computer_grid, num_ships):
             elif result == 'miss':
                 print("The computer missed.")
             else:
-                continue  # Computer guessed a cell it already guessed
+                continue
 
             current_player = 'player'
 
@@ -142,9 +159,12 @@ def play_game(player_grid, computer_grid, num_ships):
 
 
 
-
+        
+#Validation des entrées :
+#Mise à jour de la grille après les attaques 
+# problenme lancement du jeux??
 
 start_game()
-
+play_game(player_grid, computer_grid, num_ships)
 
 
